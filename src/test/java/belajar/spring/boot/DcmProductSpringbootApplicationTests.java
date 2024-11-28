cb6af6af254d262164be491e211826c04803e0a8
unknown <fajardillah25@gmail.com>
2024-11-28 21:21:56 +0700

    Upload

---
+++ src/test/java/belajar/spring/boot/DcmProductSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package belajar.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcmProductSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
